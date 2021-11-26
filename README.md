f2333fc8959628d90eea834b06264c1515a0bbc6
Thach <ducthach.1401@gmail.com>
2021-11-26 22:31:55 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Assignment 2
+
+Nodejs 10
+
+Cai dat xong chay: npm install
+
+Tiep theo: npm run dev
--- src/model/model.js
+++ src/model/model.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
 
 var database = mysql.createConnection({
     host: process.env.SERVER,
@@ -14,9 +16,66 @@ database.connect(function(err) {
     console.log("Connected!!!")
 });
 
-module.exports.login = async (payload) => {
+module.exports.login = async(data) =>{
     try {
-        
+        const cmd_query = 'SELECT * FROM Client WHERE Username = "{}";'.replace('{}', data.username);
+        const user = await database.query(cmd_query, (err, result) => {
+            if (err){
+                throw err;
+            }
+            if (result.length <= 0){
+                return {
+                    message: "Username or password wrong!!", 
+                }
+            }
+            console.log(result);
+
+        });
+    
+        // const result = bcrypt.compareSync(data.password, user.password);
+        if (result){
+            const payload = {
+                _id: user._id,
+                username: user.username,
+                roleUser: user.roleUser
+            }
+            const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '6h'});
+            const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET);
+            user.refreshToken = refreshToken;
+            user.save();
+            return {
+                accessToken: accessToken,
+                refreshToken: refreshToken
+            }
+        }
+        else {
+            return {
+                message: "Password wrong"
+            }
+        }
+    } catch (error) {
+        throw error;   
+    }
+}
+
+module.exports.regenerateAccessToken = async (refreshToken) => {
+    try {
+        const user = await User.findOne({refreshToken: refreshToken});
+        if (user) {
+            const payload = {
+                _id: user._id,
+                username: user.username,
+                roleUser: user.roleUser
+            }
+            const userRefresh = await jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+            const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '6h'});
+            return {
+                accessToken: accessToken
+            }
+        }
+        else {
+            return null;
+        }
     } catch (error) {
         throw error;
     }
@@ -38,9 +97,26 @@ module.exports.refreshToken = async (token) => {
     }
 }
 
-module.exports.getAllClients = async (data) => {
+module.exports.getAllClients = async () => {
     try {
-        
+        const cmd_query = 'SELECT * FROM Client';
+        const client = await database.query(cmd_query, (err, result) => {
+            if (err){
+                throw err;
+            }
+            if (result.length == 0){
+                return {
+                    message: 'Data null'
+                };
+            }
+            for (let data of result){
+                delete data.Password;
+            }
+            return {
+                message: 'Success',
+                data: result
+            }
+        })
     } catch (error) {
         throw error;
     }
@@ -48,7 +124,24 @@ module.exports.getAllClients = async (data) => {
 
 module.exports.getClient = async (data)  => {
     try {
-        
+        const cmd_query = 'SELECT * FROM Client WHERE LOCATE("{}", Client_Name) > 0;'.replace('{}', data.name);
+        const client = await database.query(cmd_query, (err, result) => {
+            if (err){
+                throw err;
+            }
+            if (result.length == 0){
+                return {
+                    message: 'Data null'
+                };
+            }
+            for (let data of result){
+                delete data.Password;
+            }
+            return {
+                message: 'Success',
+                data: result
+            }
+        })
     } catch (error) {
         throw error;
     }
@@ -56,7 +149,21 @@ module.exports.getClient = async (data)  => {
 
 module.exports.billClient = async (data) => {
     try {
-        
+        const cmd_query = 'SELECT * FROM Client WHERE Client_ID = "{}";'.replace('{}', data.id);
+        const client = await database.query(cmd_query, (err, result) => {
+            if (err){
+                throw err;
+            }
+            if (result.length == 0){
+                return {
+                    message: 'Data null'
+                };
+            }
+            return {
+                message: 'Success',
+                data: result
+            }
+        })
     } catch (error) {
         throw error;
     }
@@ -77,7 +184,3 @@ module.exports.totalClients = async (data) => {
         throw error;
     }
 }
-
-// database.query('SELECT * FROM Client', (err, result) => {
-//     console.log(result);
-// });
